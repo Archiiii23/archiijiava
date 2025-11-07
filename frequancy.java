b883fcfb93f109c4a3e2b94e47ea12790b2ecded
Archiiii23 <jainarchi555@gmail.com>
2025-11-07 13:18:33 +0530

    Add files via upload

---
+++ frequancy.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class frequancy {
+    public static void main(String[] args) {
+        String str="abc";
+        int freq[]=new int [256];
+        for (char c:str.toCharArray()){
+            freq[c]++;
+        }
+        for (int i=0;i<256;i++){
+            if (freq[i]>0){
+                System.out.println((char)i+ "->"+ freq[i]);
+            }
+        }
+    }
+}
