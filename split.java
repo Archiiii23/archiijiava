d1106b9c7eed9904d4d2cbb88020a8affbd8789c
Archiiii23 <jainarchi555@gmail.com>
2025-11-07 12:39:06 +0530

    Add files via upload

---
+++ split.java
@@ -0,0 +1,12 @@
+public class split {
+    class Main{
+        public static void main(String[] args){
+
+            String str1 = "java cpp c php is";
+            String a[]=str1.split("");
+           for (String value:a){
+               System.out.println(value);
+           }
+        }
+    }
+}
