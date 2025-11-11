4d7a9d6386fcb93e5ae531a6fbb58ff606628149
Archiiii23 <jainarchi555@gmail.com>
2025-11-11 15:16:43 +0530

    Add files via upload

---
+++ sumstring.java
@@ -0,0 +1,10 @@
+public class sumstring {
+    static void sum() {
+        System.out.println("Archiiiiiiiiiiiiiiiiiiiiiii");
+    }
+    public static void main(String[] args) {
+        sumstring obj= new sumstring();
+        obj.sum();
+    }
+}
+
