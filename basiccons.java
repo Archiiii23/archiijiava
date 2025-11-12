3bd98378ff503ef3c38378579e73742cf845046d
Archiiii23 <jainarchi555@gmail.com>
2025-11-12 20:46:39 +0530

    Add files via upload

---
+++ basiccons.java
@@ -0,0 +1,13 @@
+class basiccons{
+    basiccons(int a){
+        System.out.println("hii");
+
+    }
+}
+class s{
+    public static void main (String[] args){
+        basiccons c1=new basiccons(5);
+    }
+}
+
+
