aaa38f7ef8f01e3d084e0dee10fb8c9ba18befa8
Archiiii23 <jainarchi555@gmail.com>
2025-11-26 14:17:15 +0530

    Add files via upload

---
+++ hashmap3.java
@@ -0,0 +1,19 @@
+// sum print karana h to uska index 9 hona chaiye//
+import java.util.HashMap;
+class hashmap3 {
+    public static void main(String[] args) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int arr[]={2,7,11,15};
+        int target=9;
+
+        for(int i=0;i<arr.length;i++){
+            int diff=target-arr[i];
+            if(map.containsKey(diff)){
+                System.out.println(map.get(diff)+ " "+i);
+                break;
+            }
+            map.put(arr[i], i);
+        }
+    }
+}
